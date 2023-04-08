4348b28182dc5fa2f954b5ec7344c74a5eff122b
Md. Almas Ali <almaspr3@gmail.com>
2023-04-08 08:28:37 +0600

    Fixed autoreload server issue, added flexible routing, added middleware support.

--- src/backkr/__init__.py
+++ src/backkr/__init__.py
@@ -1,3 +1,9 @@
+from backkr.urls import Router, Request, Response
+from backkr.template import Template
+
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+from http import HTTPStatus
+import traceback
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 from backkr.http import Autoload
@@ -9,6 +15,8 @@ import time
 
 from wsgiref.simple_server import make_server
 import threading
+
+import logging
 
 
 # class Server:
@@ -74,14 +82,29 @@ class ServerX:
         self.debug: bool = debug
         self.watched_extensions: List[str] = ['.py']
         self.watched_files: List[str] = []
-        self.path: str = path
+        self.path: str = os.path.dirname(os.path.abspath(path))
         self.server = make_server(host, port, self.handle_request)
 
     def handle_request(self, environ, start_response):
         path = environ['PATH_INFO']
         query = parse_qs(environ['QUERY_STRING'])
         request = environ
-        response = router(path)(request)
+        template = Template()
+        try:
+            # Send the request to the router
+            response = router(path)['controller'](request)
+
+        except KeyError:
+            # If route not found
+            response = template.Error404()
+
+        except TypeError as TE:
+            # If request not passed in controller
+            if self.debug:
+                response = template.ErrorWithResponse(response=str(TE))
+            else:
+                response = template.Error500()
+
         start_response('200 OK', [('Content-type', 'text/html')])
         # print(f'path: {path}, request: {request}, response: {response}')
         return [bytes(response, 'utf-8')]
@@ -101,9 +124,9 @@ class ServerX:
         """
         Reloads the server by gracefully shutting down and then restarting.
         """
-        print('Reloading server...')
+        print('Reloading server...\n')
         time.sleep(1)  # Give time for previous connections to close.
-        os.execv(sys.executable, [sys.executable] + sys.argv)
+        os.execv(sys.executable, [sys.executable] + sys.argv) # Restart the server.
 
     def check_file_changes(self):
         """
@@ -111,12 +134,15 @@ class ServerX:
         If a file has changed, the server is reloaded.
         """
         while True:
+            # print('Checking for file changes...')
             for file in self.watched_files:
+                # print(file)
                 if os.stat(file).st_mtime > self.start_time:
                     self.reload_server()
+                    print('File changed: ' + file)
             time.sleep(1)
-            
-    def is_watched_file(self, filename):
+
+    def is_watching_file(self, filename):
         """
         Checks if a filename has an extension in self.watched_extensions.
         """
@@ -126,11 +152,17 @@ class ServerX:
         """
         Recursively finds all Python files in a directory and its subdirectories.
         """
-        for root, dirs, files in os.walk(dir_path):
-            for file in files:
-                if self.is_watched_file(file):
-                    self.watched_files.append(os.path.join(root, file))
-    
+        for file in os.listdir(dir_path):
+            if file == '__main__':
+                continue
+            file_path = os.path.join(dir_path, file)
+
+            if os.path.isdir(file_path):
+                self.find_watched_files(file_path)
+
+            elif self.is_watching_file(file_path):
+                self.watched_files.append(file_path)
+
     def run(self):
         """
         Runs the server.
@@ -139,37 +171,27 @@ class ServerX:
         self.find_watched_files(self.path)
 
         try:
-            _thread = threading.Thread(
+            self._thread0 = threading.Thread(
                 target=self.server.serve_forever)
-            _thread.start()
+            self._thread0.start()
 
+            print('Backkr Development Server')
             print(f'Serving at http://{self.host}:{self.port}')
             print('Press Ctrl+C to stop')
 
-            _thread1 = threading.Thread(
+            self._thread1 = threading.Thread(
                 target=self.check_file_changes, daemon=True)
-            _thread1.start()
-            # print('Watching for file changes...')
+            self._thread1.start()
+            print('Watching for file changes...')
 
-            # _thread.join()
-            # _thread1.join()
+            self._thread0.join()
+            self._thread1.join()
 
         except KeyboardInterrupt:
-            print('Server stopped')
+            print('\nServer stopped')
 
         except Exception as e:
             print(e)
-
-
-@dataclass
-class request:
-    path: str
-    query: Dict[str, Any]
-    cookies: Dict[str, Any]
-    headers: Dict[str, Any]
-    body: Dict[str, Any]
-    session: Dict[str, Any]
-
 
 # class RequestHandler(BaseHTTPRequestHandler):
 
@@ -237,20 +259,99 @@ class request:
 #             print('Something went wrong with server ', e)
 
 
+# class Backkr:
+#     def __init__(self, file_path: str) -> None:
+#         self.__file_path = file_path
+
+#         logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.DEBUG)
+#         handler = logging.StreamHandler()
+#         handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s'))
+#         logging.getLogger().handlers = [handler]
+
+#     @property
+#     def router(self):
+#         global router
+#         return router
+
+#     @router.setter
+#     def router(self, value):
+#         global router
+#         router = value
+
+#     def run(self, debug: bool = True, host: str = '127.0.0.1', port: int = 8000) -> None:
+#         server = ServerX(debug, host, port, self.__file_path)
+#         server.run()
+
+
 class Backkr:
-    def __init__(self, file_path: str) -> None:
-        self.__file_path = file_path
-
-    @property
-    def router(self):
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.router = Router()
         global router
-        return router
+        router = self.router
+        self.middlewares: List[Callable] = []
 
-    @router.setter
-    def router(self, value):
-        global router
-        router = value
+    def __call__(self, environ: Dict[str, Any], start_response: Callable) -> Any:
+        request = Request(environ)
+        response = self.handle_request(request)
+        return response(environ, start_response)
 
-    def run(self, debug: bool = True, host: str = '127.0.0.1', port: int = 8000) -> None:
-        server = ServerX(debug, host, port, self.__file_path)
+    def handle_request(self, request: Request) -> Response:
+        response = Response()
+        try:
+            response = self.apply_middleware(request, response)
+            if response.status_code == HTTPStatus.CONTINUE:
+                print(response.status_code)
+                return response
+            handler = self.router.resolve(request)
+            response = handler(request)
+        except Exception as e:
+            response = self.handle_exception(request, e)
+
+        return response
+
+    def handle_exception(self, request: Request, exception: Exception) -> Response:
+        trace = traceback.format_exc()
+        response = Response(
+            status=HTTPStatus.INTERNAL_SERVER_ERROR,
+            content_type="text/plain",
+            body=trace.encode(),
+        )
+        return response
+
+    def apply_middleware(self, request: Request, response: Response) -> Response:
+        for middleware in self.middlewares:
+            response = middleware(request, response)
+            if response.status_code != HTTPStatus.CONTINUE:
+                break
+        return response
+
+    def add_middleware(self, middleware: Callable) -> None:
+        self.middlewares.extend(middleware)
+
+    def route(self, path: str, methods: Union[str, List[str], Dict[str, str]] = "GET") -> Callable:
+        # print(router.show_routes())
+
+        def wrapper(func: Callable) -> Callable:
+            self.router.add_route(path, func, methods)
+            return func
+
+        return wrapper
+
+    def run(
+        self,
+        host: str = "localhost",
+        port: int = 8000,
+        debug: bool = False,
+        **options: Any,
+    ) -> None:
+        from wsgiref.simple_server import make_server
+
+        server = ServerX(
+            host=host,
+            port=port,
+            debug=debug,
+            path=self.name,
+            **options,
+        )
         server.run()
