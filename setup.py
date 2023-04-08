d3397f6181108ad2de8355b58c9f3ef5421c5cdc
Md. Almas Ali <almaspr3@gmail.com>
2023-04-08 08:50:33 +0600

    Updated setup file for version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='backkr',
-    version='0.0.1',
+    version='0.0.2',
     description='A backend framework the web',
     long_description_content_type="text/markdown",
     long_description=long_description,
